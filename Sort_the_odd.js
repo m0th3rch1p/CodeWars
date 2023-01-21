7efd10da275a0c75fcb58b0aaa972841e6559839
m0th3rch1p <njagibret@gmail.com>
2023-01-21 04:26:27 +0300

    Answer

--- Sort_the_odd.js
+++ Sort_the_odd.js
@@ -7,3 +7,13 @@ Examples
 [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 */
 
+function sortArray(array) {
+    let oddNumbers = array.filter(num => num % 2 !== 0).sort((a, b) => a - b);
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] % 2 !== 0) {
+        array[i] = oddNumbers.shift();
+      }
+    }
+    return array;
+  }
+  
